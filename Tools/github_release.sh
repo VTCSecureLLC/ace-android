eb9a7e1aa8ad6f1d3cdcb44d03721e3f08eb5a3d
Ian Blenke <ian@blenke.com>
2015-09-20 00:39:38 -0400

    Add a guard before github release

--- Tools/github_release.sh
+++ Tools/github_release.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -x
 
+if [ ! -f bin/Linphone-debug.apk ]; then
+  echo "Could not find a bin/Linphone-debug.apk to publish"
+  exit 1
+fi
+
 curl -sL https://github.com/aktau/github-release/releases/download/v0.5.3/darwin-amd64-github-release.tar.bz2 | bunzip2 -cd | tar xf - --strip=3 -C /tmp/
+
+chmod 755 /tmp/github-release
 
 tag="$(bundle exec semver)-${TRAVIS_BUILD_NUMBER:-1}"-$(git rev-parse --short HEAD)
 
