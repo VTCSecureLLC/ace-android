4d1a3b4b31eaab677b4c0b1443fde64719cde155
zack matthews <zack@zackmatthews.com>
2015-09-22 13:54:23 -0400

    Reverted LinphoneActivity.java

--- src/org/linphone/LinphoneActivity.java
+++ src/org/linphone/LinphoneActivity.java
@@ -35,7 +35,6 @@ import org.linphone.core.LinphoneCall;
 import org.linphone.core.LinphoneCall.State;
 import org.linphone.core.LinphoneCallLog;
 import org.linphone.core.LinphoneCallLog.CallStatus;
-
 import org.linphone.core.LinphoneChatMessage;
 import org.linphone.core.LinphoneChatRoom;
 import org.linphone.core.LinphoneCore;
@@ -1009,7 +1008,6 @@ public class LinphoneActivity extends FragmentActivity implements OnClickListene
 			int rotation = (360 - degrees) % 360;
 			LinphoneCore lc = LinphoneManager.getLcIfManagerNotDestroyedOrNull();
 			if (lc != null) {
-
 				lc.setDeviceRotation(rotation);
 				LinphoneCall currentCall = lc.getCurrentCall();
 				if (currentCall != null && currentCall.cameraEnabled() && currentCall.getCurrentParamsCopy().getVideoEnabled()) {
@@ -1279,6 +1277,7 @@ public class LinphoneActivity extends FragmentActivity implements OnClickListene
 		return super.onKeyDown(keyCode, event);
 	}
 }
+
 interface ContactPicked {
 	void setAddresGoToDialerAndCall(String number, String name, Uri photo);
 	void goToDialer();
