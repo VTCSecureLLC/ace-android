b612bb5d9143c3565feac8a128de3b4b1847ffee
zack matthews <zack@zackmatthews.com>
2015-09-25 10:34:19 -0400

    Added crashlytics build dependencies

---
+++ kit-libs/com-crashlytics-sdk-android_crashlytics-core/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/zackmatthews/github.com/accounts/VTCSecure/ace-android/kit-libs/com-crashlytics-sdk-android_crashlytics-core/bin/AndroidManifest.xml : \
+/Users/zackmatthews/github.com/accounts/VTCSecure/ace-android/kit-libs/com-crashlytics-sdk-android_crashlytics-core/AndroidManifest.xml \
---
+++ kit-libs/io-fabric-sdk-android_fabric/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/zackmatthews/github.com/accounts/VTCSecure/ace-android/kit-libs/io-fabric-sdk-android_fabric/bin/AndroidManifest.xml : \
+/Users/zackmatthews/github.com/accounts/VTCSecure/ace-android/kit-libs/io-fabric-sdk-android_fabric/AndroidManifest.xml \
