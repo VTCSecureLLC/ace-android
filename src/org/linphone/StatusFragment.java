680fa45c5dfec75b572bdc9a3b02688aeec3429b
patrickmwatson <patrickmwatson@gmail.com>
2016-04-27 14:40:25 -0400

    VATRP-3912 Android: Hide username at top of screen

--- src/org/linphone/StatusFragment.java
+++ src/org/linphone/StatusFragment.java
@@ -321,7 +321,7 @@ public class StatusFragment extends Fragment {
 						if(addr != null && LinphoneManager.getLc().getCallsNb() == 0){
 							String addrText = addr.asString();
 							if(addrText != null) {
-								userNameText.setVisibility(View.VISIBLE);
+								userNameText.setVisibility(View.INVISIBLE);
 								userNameText.setText(addrText.replace("sip:",""));
 								callQuality.setVisibility(View.INVISIBLE);
 							}
